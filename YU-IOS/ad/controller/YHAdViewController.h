81a752338cf7c10dfc9cb6f7121f5407f012334f
yuhao <935513693@qq.com>
2017-10-12 18:49:47 +0800

    1.0

---
+++ YU-IOS/ad/controller/YHAdViewController.h
@@ -0,0 +1,14 @@
+//
+//  YHAdViewController.h
+//  YU-IOS
+//
+//  Created by yuhao on 2017/10/12.
+//  Copyright © 2017年 yuhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YHAdViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *adBel;
+
+@end
---
+++ YU-IOS/ad/controller/YHAdViewController.m
@@ -0,0 +1,26 @@
+//
+//  YHAdViewController.m
+//  YU-IOS
+//
+//  Created by yuhao on 2017/10/12.
+//  Copyright © 2017年 yuhao. All rights reserved.
+//
+
+#import "YHAdViewController.h"
+
+@interface YHAdViewController ()
+
+@end
+
+@implementation YHAdViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (IBAction)SkipBtn:(id)sender {
+    
+}
+
+@end
