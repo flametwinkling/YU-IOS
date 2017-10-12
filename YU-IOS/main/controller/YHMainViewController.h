ded66d447d09cad7a24709e3bcd411ded5719762
yuhao <935513693@qq.com>
2017-10-12 17:18:09 +0800

    1.0

---
+++ YU-IOS/main/controller/YHMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  YHMainViewController.h
+//  YU-IOS
+//
+//  Created by yuhao on 2017/10/12.
+//  Copyright © 2017年 yuhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YHMainViewController : UIViewController
+
+@end
---
+++ YU-IOS/main/controller/YHMainViewController.m
@@ -0,0 +1,23 @@
+//
+//  YHMainViewController.m
+//  YU-IOS
+//
+//  Created by yuhao on 2017/10/12.
+//  Copyright © 2017年 yuhao. All rights reserved.
+//
+
+#import "YHMainViewController.h"
+@interface YHMainViewController ()
+
+@end
+
+@implementation YHMainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+    
+}
+
+
+@end
