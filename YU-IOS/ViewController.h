790f617176007d1207253b54f3e26ae9776d41d6
yuhao <935513693@qq.com>
2017-10-12 15:13:56 +0800

    Initial Commit

---
+++ YU-IOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YU-IOS
+//
+//  Created by yuhao on 2017/10/12.
+//  Copyright © 2017年 yuhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YU-IOSTests/YU_IOSTests.m
@@ -0,0 +1,39 @@
+//
+//  YU_IOSTests.m
+//  YU-IOSTests
+//
+//  Created by yuhao on 2017/10/12.
+//  Copyright © 2017年 yuhao. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface YU_IOSTests : XCTestCase
+
+@end
+
+@implementation YU_IOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
